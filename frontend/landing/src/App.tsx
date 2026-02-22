dababead99bd9d6c6f681af5af61dfc4b6d70783
Tejit <35284488+tejitpabari99@users.noreply.github.com>
2026-02-22 18:08:47 +0000

    beta list page (#45)
    * beta list page
    
    * Merge conflicts fix

--- frontend/landing/src/App.tsx
+++ frontend/landing/src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import DementiaPage from './pages/DementiaPage'
+import BetaPage from './BetaPage'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/beta" element={<BetaPage />} />
       <Route path="/dementia" element={<DementiaPage />} />
     </Routes>
   )

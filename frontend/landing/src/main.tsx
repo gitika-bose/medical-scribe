b73c84b6c34352fed5b315190e3c55465c40e966
tejitpabari99 <tejitpabari99@gmail.com>
2026-02-22 18:08:30 +0000

    Merge conflicts fix

--- frontend/landing/src/main.tsx
+++ frontend/landing/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import BetaPage from './BetaPage.tsx'
+import DementiaPage from './pages/DementiaPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/beta" element={<BetaPage />} />
+        <Route path="/dementia" element={<DementiaPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
